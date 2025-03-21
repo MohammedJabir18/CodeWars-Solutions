254b8b23e9300c9435e47cc31f73f7559ffc73a4
Mohammed Jabir <jabirahmedz111@gmail.com>
2025-03-21 13:55:48 +0530

    Update solution - CodeHub

--- 7-kyu/regex-validate-pin-code/regex-validate-pin-code.py
+++ 7-kyu/regex-validate-pin-code/regex-validate-pin-code.py
@@ -1,5 +1,2 @@
 def validate_pin(pin):
-    if (len(pin) == 4 or len(pin) == 6) and pin.isdigit():
-        return True
-    else:
-        return False
+    return len(pin) in (4, 6) and pin.isdigit()
