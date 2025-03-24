817ef7c2785c70c9c961e7e91f03f2becd05a411
Mohammed Jabir <jabirahmedz111@gmail.com>
2025-03-24 17:35:43 +0530

    highest-and-lowest problem hint added

--- 7-kyu/highest-and-lowest/highest-and-lowest.py
+++ 7-kyu/highest-and-lowest/highest-and-lowest.py
@@ -1,3 +1,6 @@
 def high_and_low(numbers):
+    # numbers = [int(x) for x in number.split()]
+
+    # numbers = f"{min(numbers)} {max(numbers)}"
     
     return f"{max([int(x) for x in numbers.split()])} {min([int(x) for x in numbers.split()])}"
