e35e01224cf397371ae19fc98b94d3005988778c
Mohammed Jabir <jabirahmedz111@gmail.com>
2025-03-26 13:47:10 +0530

    Update solution - CodeHub

--- 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.py
+++ 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.py
@@ -1,7 +1,2 @@
 def domain_name(url):
-    url = (url.split("://")[-1]).split("/")[0]
-    
-    if url.startswith("www."):
-        url = url[4:]
-        
-    return url.split(".")[0]
+    return (((url.split("//")[-1]).split("www.")[-1]).split(".")[0])
